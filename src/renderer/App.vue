00a57bd76a6c5654b848cf39e64359c421e35b08
RamboWu <RamboWu90@gmail.com>
2017-09-29 10:56:46 +0800

    增加点击微信的接口

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -5,6 +5,7 @@ import Card from './components/card';
 import List from './components/list';
 import TextChat from './components/textchat';
 import Message from './components/message';
+const ipcRenderer = require('electron').ipcRenderer;
 
 export default {
     name: 'proxy_wechat',
@@ -16,6 +17,9 @@ export default {
             'selectSession',
             'search',
         ]),
+        request_login_wechat: () => {
+            ipcRenderer.send('request_login');
+        },
     },
     created() {
         this.initData();
@@ -29,6 +33,8 @@ export default {
     <div class="sidebar">
         <card></card>
         <list></list>
+        <button type="button" @click="request_login_wechat">登录微信</button>
+        <button type="button">后台管理</button>
     </div>
     <div class="main">
         <message></message>
